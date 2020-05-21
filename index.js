862bad0bbccb6c1476db478611d641954b57eb32
Nabil-MEDIOUNI <nabilmediouni2000@gmail.com>
2020-05-21 03:24:12 +0100

    adding comments so the code become more readable

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const eps = [
   { name: "chawki", age: "24", status: "open", lc: "medina" },
 ];
 
+//clback funtion brings map through the elements of the array
 const names = eps.map((ep) => ep.name);
 
-const epFromMedina = eps.filter((ep) => ep.lc !== "medina");
+//clback funtion brings all objects with the filtering condtion
+const epFromMedina = eps.filter((ep) => ep.lc == "medina");
+const epFromOtherLcs = eps.filter((ep) => ep.lc !== "medina");
 
-const epStatusOpen = eps.find((ep) => ep.name === "chawki");
+//clback funtion brings the object once the condition is true
+const epStatusOpen = eps.find((ep) => ep.status === "open");
 
+//clback funtion that takes the previous and the next element of the array and combine them
 const totalAgesEps = eps.reduce((prev, curr) => {
   return prev + parseInt(curr.age);
 }, 0);
--- reduce.js
+++ reduce.js
@@ -1,13 +1,19 @@
+//importing package to read the file
 const fs = require("fs");
 
+//transform the file content from <Buffer></Buffer> to normal text
 const output = fs.readFileSync("./eps.txt", "utf-8");
 
+//removing spaces using trim(), spliting lines with split()
 const objects = output
   .trim()
   .split("\n")
   .map((line) => line.split("\t"));
 
-const ep_details = ((objects, selected_programme) => {
+//taking the first element as a header of the arrays and push desired objects within 
+//you can find {name, selected_programme, age}
+//this is function that takes array as an argument
+const transformArray = ((array) => {
     return objects.reduce((prev, curr) => {
         prev[curr[0]] = prev[curr[0]] || [];
         prev[curr[0]].push({
@@ -17,6 +23,10 @@ const ep_details = ((objects, selected_programme) => {
         });
         return prev;
       }, {})
-      .ep_info.filter((ep) => ep.selected_programme === selected_programme)
-      .map((ep) => ep);
 })
+
+//getting ep_details from the array
+const ep_details = transformArray(objects).ep_details
+
+//getting ep_info from the array
+const ep_info = transformArray(objects).ep_info

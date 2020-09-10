348aecf6c5bb1b124de73745f91f58546de443fb
Nabil-MEDIOUNI <nabilmediouni2000@gmail.com>
2020-09-10 14:30:38 +0100

    change eps to people

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const eps = [
-  { name: "chokri", age: "20", status: "applied", lc: "medina" },
-  { name: "ayari", age: "24", status: "open", lc: "university" },
-  { name: "hsan", age: "19", status: "applied", lc: "bardo" },
-  { name: "mounir", age: "28", status: "finished", lc: "medina" },
-  { name: "chawki", age: "24", status: "open", lc: "medina" },
+const people = [
+  { name: "chokri", age: "20", country: "tunisia" },
+  { name: "ivan", age: "24", country: "france" },
+  { name: "jhon", age: "19", country: "usa" },
+  { name: "aya", age: "28", country: "tunisia" },
+  { name: "chawki", age: "24", country: "tunisia" },
 ];
 
 //clback funtion brings map through the elements of the array
-const names = eps.map((ep) => ep.name);
+const names = people.map((person) => person.name);
 
 //clback funtion brings all objects with the filtering condtion
-const epFromMedina = eps.filter((ep) => ep.lc == "medina");
-const epFromOtherLcs = eps.filter((ep) => ep.lc !== "medina");
+const peopleFromTunisia = people.filter(
+  (person) => person.country === "tunisia"
+);
+const peopleFromOtherLcs = people.filter(
+  (person) => person.country !== "tunisia"
+);
 
 //clback funtion brings the object once the condition is true
-const epStatusOpen = eps.find((ep) => ep.status === "open");
+const personStatusOpen = people.find((person) => person.status === "open");
 
 //clback funtion that takes the previous and the next element of the array and combine them
-const totalAgesEps = eps.reduce((prev, curr) => {
+const totalAges = people.reduce((prev, curr) => {
   return prev + parseInt(curr.age);
 }, 0);
--- reduce.js
+++ reduce.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 
 //transform the file content from <Buffer></Buffer> to normal text
-const output = fs.readFileSync("./eps.txt", "utf-8");
+const output = fs.readFileSync("./people.txt", "utf-8");
 
 //removing spaces using trim(), spliting lines with split()
 const objects = output
@@ -10,23 +10,23 @@ const objects = output
   .split("\n")
   .map((line) => line.split("\t"));
 
-//taking the first element as a header of the arrays and push desired objects within 
-//you can find {name, selected_programme, age}
+//taking the first element as a header of the arrays and push desired objects within
+//you can find {name, study_at, age}
 //this is function that takes array as an argument
-const transformArray = ((array) => {
-    return objects.reduce((prev, curr) => {
-        prev[curr[0]] = prev[curr[0]] || [];
-        prev[curr[0]].push({
-          name: curr[1],
-          selected_programme: curr[2],
-          age: curr[3],
-        });
-        return prev;
-      }, {})
-})
+const transformArray = (array) => {
+  return objects.reduce((prev, curr) => {
+    prev[curr[0]] = prev[curr[0]] || [];
+    prev[curr[0]].push({
+      name: curr[1],
+      study_at: curr[2],
+      age: curr[3],
+    });
+    return prev;
+  }, {});
+};
 
-//getting ep_details from the array
-const ep_details = transformArray(objects).ep_details
+//getting person_details from the array
+const person_details = transformArray(objects).person_details;
 
-//getting ep_info from the array
-const ep_info = transformArray(objects).ep_info
+//getting person_info from the array
+const person_info = transformArray(objects).person_info;

da5ec3eaa678ed200ccf8d08bb116a3bbefd9e12
Nabil-MEDIOUNI <nabilmediouni8@gmail.com>
2021-03-11 22:27:05 +0100

    master/ mini workshop with malikk

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const people = [
-  { name: "chokri", age: "20", country: "tunisia" },
-  { name: "ivan", age: "24", country: "france" },
-  { name: "jhon", age: "19", country: "usa" },
-  { name: "aya", age: "28", country: "tunisia" },
-  { name: "chawki", age: "24", country: "tunisia" },
+var people = [
+  { name: "alan", age: 20, country: "france" },
+  { name: "ivan", age: 24, country: "tunisia" },
+  { name: "jhon", age: 19, country: "usa" },
+  { name: "aya", age: 28, country: "tunisia" },
+  { name: "nabil", age: 24, country: "tunisia" },
 ];
 
-//clback funtion brings map through the elements of the array
-const names = people.map((person) => person.name);
+//--------------------map---------------------------//
+var array = [];
+for (var i = 0; i < people.length; i++) {
+  array.push({
+    name: people[i].name,
+    age: people[i].age,
+  });
+}
 
-//clback funtion brings all objects with the filtering condtion
-const peopleFromTunisia = people.filter(
-  (person) => person.country === "tunisia"
-);
-const peopleFromOtherLcs = people.filter(
-  (person) => person.country !== "tunisia"
-);
+const array1 = people.map(function (person) {
+  return person.name
+});
 
-//clback funtion brings the object once the condition is true
-const personStatusOpen = people.find((person) => person.status === "open");
+const array2 = people.map(p => {
+  return {name: p.name, age: p.age}
+});
 
-//clback funtion that takes the previous and the next element of the array and combine them
-const totalAges = people.reduce((prev, curr) => {
-  return prev + parseInt(curr.age);
-}, 0);
+// console.log(array2);
+//--------------------map---------------------------//
+
+//--------------------filter---------------------------//
+var people1 = [];
+for (let i = 0; i < people.length; i++) {
+  if(people[i].age < 24) {
+    people1.push(people[i]);
+  }
+}
+
+const people2 = people.filter(function (person) { 
+  return person.age < 24
+})
+
+const people3 = people.filter(p => p.age < 24);
+const people4 = people.filter(p => !(p.age < 24));
+
+// console.log(people3);
+//--------------------filter---------------------------//
+
+//--------------------find---------------------------//
+const findPersonFromtunisia = people.find((p) => p.country === 'tunisia')
+// console.log(findPersonFromtunisia)
+//--------------------find---------------------------//
